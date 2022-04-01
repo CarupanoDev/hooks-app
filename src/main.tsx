947dd4adb7d694259af9538dfcb6ae5bf1d9a0db
Santiago Medina <carupanodev@gmail.com>
2022-04-01 08:48:23 -0400

    106. useState

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+// import { HookApp } from "./HookApp"
+import { CounterApp } from "./components/01-useState/CounterApp"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CounterApp/>
   </React.StrictMode>,
   document.getElementById('root')
 )
