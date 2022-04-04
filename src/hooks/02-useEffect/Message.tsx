c8ce09149ab4d6538139939ffba8ee3e67700d37
Santiago Medina <carupanodev@gmail.com>
2022-04-04 08:43:17 -0400

    110. useEffect - Cautions

--- src/hooks/02-useEffect/Message.tsx
+++ src/hooks/02-useEffect/Message.tsx
@@ -1,18 +1,31 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export const Message = () => {
 
+  const [coords, setCoords] = useState({x:0, y: 0})
+  const {x, y} = coords
+
   useEffect(() => {
-    console.log('Component mounted')
+
+    const mouseMove = (e: MouseEvent) => {
+      const coords = {x: e.x, y:e.y}
+      setCoords(coords)
+    }
+
+
+    window.addEventListener('mousemove', mouseMove)
 
     return () => {
-      console.log('Component unmounted')
+      window.removeEventListener('mousemove', mouseMove)
     }
-  })
+  }, [])
 
   return(
     <div>
       <h3>You are cool!</h3>
+      <p>
+        x: {x} y: {y}
+      </p>
     </div>
   )
 }
