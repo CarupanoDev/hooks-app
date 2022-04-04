34016dd736fb3c8bb574d1feed516c62bb67e5aa
Santiago Medina <carupanodev@gmail.com>
2022-04-04 08:08:58 -0400

    107. useCounter

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
+// eslint-disable-next-line no-use-before-define
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 // import { HookApp } from "./HookApp"
-import { CounterApp } from "./components/01-useState/CounterApp"
+// import { CounterApp } from "./components/01-useState/CounterApp"
+import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook"
 
 ReactDOM.render(
   <React.StrictMode>
-    <CounterApp/>
+    <CounterWithCustomHook/>
   </React.StrictMode>,
   document.getElementById('root')
 )
